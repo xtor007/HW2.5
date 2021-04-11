edd85f712cdbd7da1d150c5b9d7399076302b75d
Anatoliy <tolxpams@gmail.com>
2021-04-11 22:04:28 +0300

    создал интерфейс

---
+++ HW2.5/Module/AllData.swift
@@ -0,0 +1,12 @@
+//
+//  AllData.swift
+//  HW2.5
+//
+//  Created by Anatoliy Khramchenko on 11.04.2021.
+//
+
+import Foundation
+
+class AllData {
+    static let data = ["user1" : "qwerty"]
+}
---
+++ HW2.5/Module/UserData.swift
@@ -0,0 +1,18 @@
+//
+//  UserData.swift
+//  HW2.5
+//
+//  Created by Anatoliy Khramchenko on 11.04.2021.
+//
+
+import Foundation
+
+class UserData {
+    
+    static var isLogNorm = false
+    static var isPasNorm = false
+    
+    func checkPas(login: String, password: String) {
+        
+    }
+}
